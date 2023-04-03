696f35d1a943b7df7ca5bd367423d76cfbd427d3
flatbignose <115427441+flatbignose@users.noreply.github.com>
2023-04-03 11:40:25 +0530

    add hive initiation and adapter registration

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:firebase_core/firebase_core.dart';
+import 'package:flip_first_build/models/user_model.dart';
 import 'package:flip_first_build/screens/flip_home.dart';
 import 'package:flip_first_build/screens/splash_screen.dart';
 import 'package:flutter/material.dart';
@@ -6,12 +7,20 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutter/services.dart';
 import 'auth/controller/auth_controller.dart';
 import 'firebase/firebase_options.dart';
+import 'models/chat_contact_model.dart';
 import 'multi_use/error.dart';
 import 'multi_use/routes.dart';
+import 'package:hive_flutter/hive_flutter.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  
+  Hive.initFlutter();
+  if (!Hive.isAdapterRegistered(UserModelAdapter().typeId)) {
+    Hive.registerAdapter(UserModelAdapter());
+  }
+  if (!Hive.isAdapterRegistered(ChatContactTileAdapter().typeId)) {
+    Hive.registerAdapter(ChatContactTileAdapter());
+  }
   SystemChrome.setPreferredOrientations(
       [DeviceOrientation.portraitUp, DeviceOrientation.portraitDown]);
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
